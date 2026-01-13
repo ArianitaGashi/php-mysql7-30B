fa4431c5454c68da102b06d5d732c70dd17275a4
ArianitaGashi <arianita.gashi@digitalschool.tech>
2026-01-13 20:50:55 +0100

    2

---
+++ module2/index.php
@@ -0,0 +1,22 @@
+<?php 
+$school = "Digital School";
+
+echo "i go to $school <hr>";
+
+
+
+$numri = 1;
+
+echo $numri.'<hr>';
+
+
+
+$x = 120;
+$y = 50;
+
+echo $x + $y.'<hr>';
+echo $x - $y.'<hr>';
+echo $x / $y.'<hr>';
+echo $x % $y.'<hr>';
+
+?>

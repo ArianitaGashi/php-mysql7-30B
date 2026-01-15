dbfcad76ded3db403b4077d99340dd1d45c396f1
ArianitaGashi <arianita.gashi@digitalschool.tech>
2026-01-15 20:50:42 +0100

    4

--- module2/index.php
+++ module2/index.php
@@ -19,4 +19,38 @@ echo $x - $y.'<hr>';
 echo $x / $y.'<hr>';
 echo $x % $y.'<hr>';
 
+$emri = "Lum";
+
+$mbiemri = "Berisha";
+
+$username = $emri.$mbiemri;
+
+echo $username. "<hr>";
+
+
+
+$theString = "Jora Vula";
+
+echo strlen($theString)."<hr>";
+
+
+
+$string5 = "We are learning php";
+
+echo str_word_count($string5)."<hr>";
+
+
+
+$string6 = "Programming is not cool";
+
+echo str_replace("not", "very", $string6)."<hr>";
+
+
+
+$string7 = "Programming";
+
+echo strrev($string7);
+
+
+
 ?>
---
+++ module3/index.php
@@ -0,0 +1,13 @@
+<?php
+$nr = 4;
+
+if($nr == 0){
+    echo "The number is 0";
+}else if($nr>0){
+    echo "The number is greater than 0";
+}else{
+    echo "The number is smaller than 0";
+}
+
+
+?>
